2ba237c0b2b996379c605bee2e514dbb8a8840ce
Enzo Sodano <117078990+SodanoEnzo@users.noreply.github.com>
2022-11-04 17:25:51 +0100

    Add files via upload

--- src/Programmatore11.java
+++ src/Programmatore11.java
@@ -7,7 +7,16 @@ public class Programmatore11 {
 		 * vettore di stringhe contare quante parole inziano con la lettera a
 		 * {ciao, ala, miao, albero} stampa 2
 		 */
-		String[] 
+		String[] vettStrings = {"ciao", "ala", "miao","albero"};
+		int cont = 0;
+		String verificaString;
+		for (int i = 0; i < vettStrings.length; i++) {
+			verificaString = vettStrings[i];
+			if (verificaString.charAt(0)== 'a') {
+				cont++;
+			}
+			System.out.println(" numero di parole che inziano con la lettera a sono " + cont);
+		}
 	}
 
 }
